599ca826d9905ac12a97d74d1d71132716212755
Yash Sharma <yashmail.sharma@gmail.com>
2017-03-19 14:30:01 +0530

    Add files via upload
    Fetching of info from local storage on refresh added.

--- script.js
+++ script.js
@@ -1,32 +1,58 @@
 window.onload = function(){
+  var txtbox=document.getElementById('txtbox');
+  var addtodo=document.getElementById('addtodo');
+  var Todolist=document.getElementById('Todolist');
   document.getElementById('txtbox').value="";
+  var arr = JSON.parse(localStorage.getItem("todos"));
+  for(var i=0;i<arr.length;i++){
+    Todolist.innerHTML +=
+    "<div>"+"<li>"+
+    arr[i].task+
+    "<button onclick='delete_todo(this)'>"+
+    "DELETE"+"</button>"+"</li>"+"</div>";
+  }
 }
 
-var txtbox=document.getElementById('txtbox');
-var addtodo=document.getElementById('addtodo');
-var Todolist=document.getElementById('Todolist');
-
+var list=[];
 
 function add() {
   Todolist.innerHTML +=
-  "<div>"+"<li>"+"<input type='checkbox' onclick='strike_todo(this.parentNode)'>"
-  +"</input>"+txtbox.value+"  "+
+  "<div>"+"<li>"+
+  txtbox.value+
   "<button onclick='delete_todo(this)'>"+
   "DELETE"+"</button>"+"</li>"+"</div>";
+  storing();
+}
+
+function storing() {
+
+  list.push({
+        task: txtbox.value
+});
+
+  localStorage.setItem("todos",JSON.stringify(list));
+
 }
 
 function delete_todo(el) {
-  console.log(this);
+  console.log(el.parentNode);
+  console.log(el.parentNode.firstChild.data);
+   deleting_from_storage(el.parentNode);
   el.parentNode.remove();
 }
 
-function strike_todo(ele) {
-  console.log(ele);
-  if(ele.style.textDecoration=="")
-  {
-    ele.style.textDecoration="line-through";
-  }
-  else{
-   ele.style.textDecoration="";
-  }
+function deleting_from_storage(el) {
+    var arr = JSON.parse(localStorage.getItem("todos"));
+    for(var i=0;i<arr.length;i++){
+      console.log(arr[i].task == el.firstChild.data);
+      if(arr[i].task == el.firstChild.data)
+      {
+          for(var j=i;j<arr.length-1;j++){
+            arr[j]=arr[j+1];
+          }
+          arr.length-=1;
+          break;
+      }
+    }
+    localStorage.setItem("todos",JSON.stringify(arr));
 }
